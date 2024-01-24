8c779dd26d9c1ea2821f123ae8a2d88f6bae4209
Burair Z <155759587+Burair22@users.noreply.github.com>
2024-01-24 13:21:18 +0500

    Add files via upload

---
+++ Lecture 7 practice questions.py
@@ -0,0 +1,22 @@
+myfavfruits=["apple","mango","grapes"]
+print(myfavfruits)
+myfavfruits.remove("grapes")
+print(myfavfruits)
+myfavfruits.append("oranges")
+print(myfavfruits)
+
+
+listodd=[1,3,5,7,9]
+listeven=[2,4,6,8,10]
+listodd.extend(listeven)
+print(listodd)
+listodd.sort() #not required aise hi kardiya
+print(listodd)
+
+
+daysofweek=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+print(daysofweek)
+print(daysofweek[2])
+
+nameagepairs=[("burair",18),("zaidi",20),("syed",22),("mohammed",24)]
+print(nameagepairs)
